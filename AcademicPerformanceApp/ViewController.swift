db731d814b93f443eb8816b076112b6e443a979b
Kirill <kirill.melnichenkoo@gmail.com>
2019-01-17 14:05:39 +0200

    Initial Commit

---
+++ AcademicPerformanceApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AcademicPerformanceApp
+//
+//  Created by kirill on 1/17/19.
+//  Copyright © 2019 kirill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
