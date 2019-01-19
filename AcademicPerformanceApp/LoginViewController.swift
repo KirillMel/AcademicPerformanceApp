7853ae39536419dee75d424426a8b38539909e33
Kirill <kirill.melnichenkoo@gmail.com>
2019-01-19 10:08:20 +0200

    Preparation. Added folders for modules, ui, data.

---
+++ AcademicPerformanceApp/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  AcademicPerformanceApp
+//
+//  Created by kirill on 1/18/19.
+//  Copyright © 2019 kirill. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    //MARK: - Otuletss
+    
+    override func viewDidLoad() {
+        
+    }
+    
+}
